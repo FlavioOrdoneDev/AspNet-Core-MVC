c33be444c1c3772d01288897ea9e904874fe0e44
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-10 16:57:55 -0300

    Adicionados os ViewComponents

---
+++ Lista-de-Tarefas/Models/ViewComponents/AdultoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lista_de_Tarefas.Models.ViewComponents
+{
+    public class AdultoViewComponent : ViewComponent
+    {
+        private readonly TarefasContexto _contexto;
+
+        public AdultoViewComponent(TarefasContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _contexto.Pessoas.Where(x => x.Idade >= 18 && x.Idade < 60).ToListAsync());
+        }
+    }
+}
---
+++ Lista-de-Tarefas/Models/ViewComponents/IdosoViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lista_de_Tarefas.Models.ViewComponents
+{
+    public class IdosoViewComponent : ViewComponent
+    {
+        private readonly TarefasContexto _contexto;
+
+        public IdosoViewComponent(TarefasContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _contexto.Pessoas.Where(x => x.Idade >= 60).ToListAsync());
+        }
+    }
+}
