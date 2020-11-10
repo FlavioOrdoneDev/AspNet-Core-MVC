a08dae6761e6e52ceefc4d5bad889dbf4a2e1026
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-10 19:35:54 -0300

    Finalizado gráfico chart.js

--- Criptomoedas/Controllers/MoedasController.cs
+++ Criptomoedas/Controllers/MoedasController.cs
@@ -18,131 +18,51 @@ namespace Criptomoedas.Controllers
             _context = context;
         }
 
-        // GET: Moedas
         public async Task<IActionResult> Index()
         {
             return View(await _context.Moedas.ToListAsync());
-        }
-
-        // GET: Moedas/Details/5
-        public async Task<IActionResult> Details(int? id)
+        }  
+        
+        public async Task<IActionResult> EscolhadeMoedas(List<Moeda> moedas)
         {
-            if (id == null)
+            foreach (var item in moedas)
             {
-                return NotFound();
+                if (item.CheckBoxMarcado == true)
+                {
+                    Moeda moeda = await _context.Moedas.FirstAsync(x => x.Id == item.Id);
+                    moeda.Quantidade++;
+                    _context.Update(moeda);
+                    await _context.SaveChangesAsync();
+                }
             }
 
-            var moeda = await _context.Moedas
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (moeda == null)
-            {
-                return NotFound();
-            }
-
-            return View(moeda);
+            return RedirectToAction(nameof(Index));
         }
 
-        // GET: Moedas/Create
+        public JsonResult DadosGrafico()
+        {
+            return Json(_context.Moedas.Select(x => new { x.Nome, x.Quantidade }));
+        }
+
+
         public IActionResult Create()
         {
             return View();
         }
-
-        // POST: Moedas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,Quantidade")] Moeda moeda)
         {
             if (ModelState.IsValid)
             {
+                moeda.Quantidade = 0;
                 _context.Add(moeda);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
             return View(moeda);
-        }
-
-        // GET: Moedas/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var moeda = await _context.Moedas.FindAsync(id);
-            if (moeda == null)
-            {
-                return NotFound();
-            }
-            return View(moeda);
-        }
-
-        // POST: Moedas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Quantidade")] Moeda moeda)
-        {
-            if (id != moeda.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(moeda);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!MoedaExists(moeda.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(moeda);
-        }
-
-        // GET: Moedas/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var moeda = await _context.Moedas
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (moeda == null)
-            {
-                return NotFound();
-            }
-
-            return View(moeda);
-        }
-
-        // POST: Moedas/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var moeda = await _context.Moedas.FindAsync(id);
-            _context.Moedas.Remove(moeda);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
+        }        
 
         private bool MoedaExists(int id)
         {
---
+++ Criptomoedas/Models/ViewComponents/MoedaViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Criptomoedas.Models.ViewComponents
+{
+    public class MoedaViewComponent : ViewComponent
+    {
+        private readonly CriptomoedaContexto _contexto;
+
+        public MoedaViewComponent(CriptomoedaContexto contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _contexto.Moedas.ToListAsync());
+        }
+    }
+}
