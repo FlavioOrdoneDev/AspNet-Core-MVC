aa735a24676c7b7e3520f082bde217c94110a7d2
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-10 12:23:17 -0300

    Primeiro commit projeto Lista de Tarefas

---
+++ Lista-de-Tarefas/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lista_de_Tarefas.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
