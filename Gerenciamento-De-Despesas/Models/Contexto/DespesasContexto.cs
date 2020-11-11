68df01d910d5fd5ce4ea812cae1696f6b606b07f
FlavioOrdoneDev <flavio.addworks@hotmail.com>
2020-11-11 13:58:51 -0300

    Adicionado projeto de Gerenciamento de Despesas

---
+++ Gerenciamento-De-Despesas/Models/Contexto/DespesasContexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gerenciamento_De_Despesas.Models.Contexto
+{
+    public class DespesasContexto : DbContext
+    {
+        public DespesasContexto(DbContextOptions<DespesasContexto> opcoes)
+            : base(opcoes) { }
+    }
+}
